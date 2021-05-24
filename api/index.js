0ed15f209e6a2ab43e9eb9d00f97ea55cd2d81c9
sudo-vaibhav <mailvaibhavchopra@gmail.com>
2021-05-24 19:13:58 +0530

    encryption works

--- api/index.js
+++ api/index.js
@@ -1,46 +1,60 @@
+const express = require("express");
+const app = express()
+app.use(express.static("uploads"))
+
+
+const crypto = require("crypto");
+const fs = require("fs")
+
 const bodyParser = require('body-parser')
 var multer = require('multer')
+
 const multerStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads");
   },
   filename: (req, file, cb) => {
-    console.log("processing filename")
-    const ext = file.mimetype.split("/")[1];
-    cb(null, `${file.filename}-${Date.now()}.${ext}`);
+    const extStartIndex = file.originalname.lastIndexOf(".")
+    cb(null, `${file.originalname.substr(0,extStartIndex)}-${Date.now()}${file.originalname.substr(extStartIndex)}`);
   },
 });
 
 
 var upload = multer({ storage: multerStorage })
-const app = require('express')()
+
+
 app.use(require('cors')())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
-let encrypted = ''
-let decrypted = ''
+const iv = crypto.randomBytes(16)
 
-app.post('/files', upload.array("collection",5),(req, res) => {
-  console.log(req.files)
+
+app.post('/files', upload.array("collection",5),async (req, res) => {
+  console.log("req body",req.body)
+  var key = req.body.key || '14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd';
+  var cipher = crypto.createCipher('aes-256-cbc', key);
+    // , iv);
+  const encryptionPromises = req.files.map(file => {
+    return new Promise(resolve => {
+      console.log("fileData:", file)
+      const input = fs.createReadStream(file.path)
+      const output = fs.createWriteStream(file.path + ".enc")
+      
+      input.pipe(cipher).pipe(output)
+      output.on("finish", () => {
+        resolve()
+      })
+    })
+  })
+
+  await Promise.all(encryptionPromises)
+
   return res.json({
     status: 'ok',
     files : req.files
   })
 })
 
-
-
-app.get('/encrypted', (req, res) => {
-  return res.json({
-    encrypted,
-  })
-})
-
-app.get('/decrypted', (req, res) => {
-  return res.json({
-    decrypted,
-  })
-})
 
 app.listen(process.env.PORT || 8000)
